51058b940da44dc31013a6ba082c58f42a656e77
lgo <lgo@black-cats.org>
2022-02-07 21:39:49 +0100

    Correct the dot mistake by deleted newline

--- hello/hello.py
+++ hello/hello.py
@@ -5,7 +5,8 @@ name = "xx"
 
 print("Hello, what's your name?")
 print("My name is", end=' ', flush=True)
-#print("name: ")
 name = sys.stdin.readline()
 
-print("Hello", name, ".", end='')
+# name ended with a newline caracter, so I need to remove it.
+name = name[:-1]
+print("Hello " + name + ".")
